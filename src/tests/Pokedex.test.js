cb8ef8c89161eda86cf68c69bca12ebbbbaf6315
Maju Domenciano <juliadomenciano@hotmail.com>
2022-03-23 19:12:13 -0300

    Requisito 6

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,19 +1,152 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import Pokedex from '../components/Pokedex';
+import pokemon from '../data';
 import renderWithRouter from './renderWithRouter';
+
+const isPokemonFavoriteById = {
+  4: false,
+  10: false,
+  23: false,
+  25: true,
+  65: false,
+  78: false,
+  143: false,
+  148: false,
+  151: false,
+};
+
+const TestId = 'next-pokemon';
 
 describe('Testando o componente About.', () => {
   it('Verifica se é exibido na tela a mensagem Encountered pokémons', async () => {
-    /*  global.fetch = async () => ({ json: async () => ({
-      ...pokemonType,
-    }) }); */
-    renderWithRouter(<Pokedex />);
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemon }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+      />,
+    );
 
-    /* const head = await screen.findByRole('heading',
-      { name: /Encountered pokémons/i, level: 2 }); */
-    const head = screen.getByText('Encountered pokémons');
+    const head = await screen.findByRole('heading',
+      { name: /Encountered pokémons/i, level: 2 });
 
     expect(head).toBeInTheDocument();
+  });
+
+  it('Verifica se exibide o próximo Pokémon quando o botão é clicado', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemon }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+      />,
+    );
+
+    const button = screen.getByTestId(TestId);
+    const buttonName = screen.getByText('Próximo pokémon');
+    expect(button).toBeInTheDocument();
+    expect(buttonName).toBeInTheDocument();
+
+    userEvent.click(button);
+    const name = screen.getByText(/Charmander/i);
+    expect(name).toBeInTheDocument();
+
+    userEvent.click(button);
+    const nextName = screen.getByText(/Caterpie/i);
+    expect(nextName).toBeInTheDocument();
+  });
+
+  it('Verifica se exibide o próximo Pokémon quando o botão é clicado', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemon }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+      />,
+    );
+
+    const button = screen.getByTestId(TestId);
+    expect(button).toBeInTheDocument();
+
+    pokemon.forEach(() => {
+      userEvent.click(button);
+    });
+
+    const firstPokemon = screen.getByText(/Pikachu/i);
+    expect(firstPokemon).toBeInTheDocument();
+  });
+
+  it('Verifica se exibide um Pokémon por vez', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemon }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+      />,
+    );
+
+    const button = screen.getByTestId(TestId);
+
+    const findPokemon = screen.getAllByTestId('pokemon-name');
+    userEvent.click(button);
+
+    expect(findPokemon).toHaveLength(1);
+  });
+
+  it('Verifica se a Pokédex tem os botões de filtro.', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemon }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+      />,
+    );
+    const num = 7;
+
+    const filters = screen.getAllByTestId('pokemon-type-button');
+    const allButton = screen.getByText('All');
+
+    expect(filters).toHaveLength(num);
+    expect(allButton).toBeInTheDocument();
+  });
+
+  it('Verifica se o filtro funciona', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemon }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+      />,
+    );
+
+    const psychicButton = screen.getAllByTestId('pokemon-type-button');
+    userEvent.click(psychicButton[4]);
+    const firstPsychic = screen.getByText(/Alakazam/i);
+    expect(firstPsychic).toBeInTheDocument();
+
+    const button = screen.getByTestId(TestId);
+    userEvent.click(button);
+    const secondPsychic = screen.getByText(/Mew/i);
+    expect(secondPsychic).toBeInTheDocument();
+  });
+
+  it('Verifica se a Pokédex contém um botão para resetar o filtro', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemon }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+      />,
+    );
+    const allButton = screen.getAllByTestId('');
+    const all = screen.getByText('All');
+
+    userEvent.click(all);
+
+    const firstNameAfterloading = screen.getByText(/Pikachu/i);
+    expect(firstNameAfterloading).toBeInTheDocument();
+
+    const button = screen.getByText('Próximo pokémon');
+
+    userEvent.click(button);
+    const secondtNameAfterloading = screen.getByText(/Charmander/i);
+    expect(secondtNameAfterloading).toBeInTheDocument();
+
+    expect(allButton).toHaveLength(1);
   });
 });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,86 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import Pokemon from '../components/Pokemon';
+import renderWithRouter from './renderWithRouter';
+
+const pokemon = {
+  id: 25,
+  name: 'Pikachu',
+  type: 'Electric',
+  averageWeight: {
+    value: '6.0',
+    measurementUnit: 'kg',
+  },
+  image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+  moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+  foundAt: [
+    {
+      location: 'Kanto Viridian Forest',
+      map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+    },
+    {
+      location: 'Kanto Power Plant',
+      map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+    },
+  ],
+  summary: 'This intelligent Pokémon roasts hard berries.',
+};
+const pokemonId = true;
+
+describe('Testando o componente About.', () => {
+  it('Verifica se renderiza um card com as informações de determinado pokémon.', () => {
+    renderWithRouter(
+      <Pokemon
+        pokemon={ pokemon }
+        isFavorite={ pokemonId }
+      />,
+    );
+
+    const name = screen.getByTestId('pokemon-name');
+    const type = screen.getByTestId('pokemon-type');
+    const weight = screen.getByTestId('pokemon-weight');
+    const averageWeight = 'Average weight: 6.0 kg';
+
+    const img = screen.getByAltText('Pikachu sprite');
+
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe(pokemon.image);
+
+    expect(name).toBeInTheDocument();
+    expect(name).toHaveTextContent('Pikachu');
+    expect(type).toBeInTheDocument();
+    expect(type).toHaveTextContent('Electric');
+    expect(weight).toBeInTheDocument();
+    expect(weight).toHaveTextContent(averageWeight);
+  });
+
+  it('Verifica se renderiza um card com as informações de determinado pokémon.', () => {
+    const { history } = renderWithRouter(
+      <Pokemon
+        pokemon={ pokemon }
+        isFavorite={ pokemonId }
+      />,
+    );
+
+    const detailsLink = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(detailsLink);
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/pokemons/25');
+  });
+
+  it('Verifica Teste se existe um ícone de estrela nos Pokémons favoritados.', () => {
+    renderWithRouter(
+      <Pokemon
+        pokemon={ pokemon }
+        isFavorite={ pokemonId }
+      />,
+    );
+
+    const star = screen.getAllByRole('img',
+      { src: '/star-icon.svg', alt: 'Pikachu is marked as favorite' });
+
+    expect(star[1]).toBeInTheDocument();
+  });
+});
